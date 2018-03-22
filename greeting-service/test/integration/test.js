7c0f21475cf918ac4f283a1c2e4648979006785d
helio-frota <00hf11@gmail.com>
2018-03-22 12:06:15 -0300

    build: adding roaster on travis build

---
+++ greeting-service/test/integration/test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const test = require('tape');
+const request = require('supertest');
+const rhoaster = require('rhoaster');
+
+const testEnvironment = rhoaster({
+  deploymentName: 'nodejs-circuit-breaker-greeting'
+});
+
+testEnvironment.deploy()
+  .then(runTests)
+  .catch(console.error);
+
+function runTests (route) {
+  test('/api/greeting', t => {
+    t.plan(1);
+    request(route)
+      .get('/api/greeting')
+      .expect(200)
+      .then(response => {
+        t.ok(response.text.includes('Hello, World!'));
+      })
+      .then(_ => t.end())
+      .catch(t.fail);
+  });
+}
---
+++ name-service/test/integration/test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const test = require('tape');
+const request = require('supertest');
+const rhoaster = require('rhoaster');
+
+const testEnvironment = rhoaster({
+  deploymentName: 'nodejs-circuit-breaker-name'
+});
+
+testEnvironment.deploy()
+  .then(runTests)
+  .then(_ => test.onFinish(testEnvironment.undeploy))
+  .catch(console.error);
+
+function runTests (route) {
+  test('/api/info', t => {
+    t.plan(1);
+    request(route)
+      .get('/api/info')
+      .expect(200)
+      .then(response => {
+        t.equal(JSON.parse(response.text).state, 'ok');
+      })
+      .then(_ => t.end())
+      .catch(t.fail);
+  });
+
+  test('/api/name', t => {
+    t.plan(1);
+    request(route)
+      .get('/api/name')
+      .expect(200)
+      .then(response => {
+        t.equal(response.text, 'World!');
+      })
+      .then(_ => t.end())
+      .catch(t.fail);
+  });
+}
